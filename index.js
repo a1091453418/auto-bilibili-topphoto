59ed569358e7b255c227f31ea7e23d9a9b15f25d
a1091453418 <48952615+a1091453418@users.noreply.github.com>
2022-01-21 11:41:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ async function painting(){
 
     //添加背景图
 //     const bgImage = await loadImage('bg.jpg');
-    const bgImage = await loadImage('yaomu.jpeg');
+    const bgImage = await loadImage('hongmo.jpeg');
     ctx.drawImage(bgImage, 0, 0, canvasSize.w, canvasSize.h);
 
     //设置文字颜色和字号，字体
